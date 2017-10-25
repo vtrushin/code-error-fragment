37b4096e5c25ade5227508e3f09e315dfcc75fdd
Vladislav Trushin <vtrushin@avito.ru>
2017-10-25 13:41:16 +0300

    Removed module

--- rollup.config.js
+++ rollup.config.js
@@ -3,51 +3,33 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { list as babelHelpersList } from 'babel-helpers';
 
-const plugins = [
-	commonjs(),
-	resolve()
-];
-
-const settings = {
+export default {
 	input: 'index.js',
+	name: 'codeErrorFragment',
+	output: {
+		file: 'build.js',
+		format: 'umd'
+	},
+	plugins: [
+		commonjs(),
+		resolve(),
+		babel({
+			exclude: 'node_modules/**',
+			presets: [
+				['es2015', {
+					modules: false
+				}],
+				'stage-3'
+			],
+			plugins: [
+				'external-helpers'
+			],
+			// fixing temporary rollup's regression, remove when https://github.com/rollup/rollup/issues/1595 gets solved
+			externalHelpersWhitelist: babelHelpersList.filter(helperName => helperName !== 'asyncGenerator'),
+		})
+	],
 	watch: {
 		include: '*.js',
 		exclude: 'node_modules/**'
 	}
-};
-
-export default [
-	// ES5 build
-	Object.assign({}, settings, {
-		name: 'codeErrorFragment',
-		output: {
-			file: 'build.js',
-			format: 'umd'
-		},
-		plugins: plugins.concat(
-			babel({
-				exclude: 'node_modules/**',
-				presets: [
-					['es2015', {
-						modules: false
-					}],
-					'stage-3'
-				],
-				plugins: [
-					'external-helpers'
-				],
-				// fixing temporary rollup's regression, remove when https://github.com/rollup/rollup/issues/1595 gets solved
-				externalHelpersWhitelist: babelHelpersList.filter(helperName => helperName !== 'asyncGenerator'),
-			})
-		)
-	}),
-
-	// ES6 module
-	Object.assign({}, settings, {
-		output: {
-			file: 'module.js',
-			format: 'es'
-		},
-		plugins
-	})
-]
+}
